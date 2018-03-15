aef26ade1437713f05783dfc9c9f5a6f5737c87f
elias <eliasrosales88@gmail.com>
2018-03-15 17:02:35 +0100

    Cambios sin importancia

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { LoginCheckService } from '../login-check.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  providers:[]
+  providers:[LoginCheckService]
 })
 export class HeaderComponent implements OnInit {
+  url:string;
   urlCheck:string;
+  data:any;
   dataCheck:any;
   mensaje:string;
-  constructor(private http: HttpClient,private router: Router){
+  loginCheck:Observable<any>;
+  loginStatus:boolean;
+  
+  constructor(private http: HttpClient,private router: Router,private loginService: LoginCheckService){
+    this.url="http://localhost:3000/api/login";
     this.urlCheck="http://localhost:3000/api/loginCheck";
+    this.loginCheck=this.loginService.loginChecker();
+    this.loginCheck.subscribe(this.manejaLoginCheck.bind(this));
     this.mensaje="";
-   }
+  }
 
   @Output() hamburguesaClickeada = new EventEmitter<any>();
   notificaHamburguesaClickeada(): void {
@@ -23,18 +33,15 @@ export class HeaderComponent implements OnInit {
     this.hamburguesaClickeada.emit();
   }
    
+
+  manejaLoginCheck(dato){
+    console.log(dato);
+    this.loginStatus=dato;
+  }
   
   ngOnInit() {
-    this.http.get(this.urlCheck,{withCredentials: true}).subscribe(
-      dataCheck =>{
-        this.dataCheck = dataCheck;
-        console.log(this.dataCheck);
-        this.mensaje="done";
-      } ,
-      err => {
-        console.log("Error occured");
-      });
+    console.log(this.loginCheck);
+    //console.log(this.loginStatus, "hola");
   }
-
 }
 

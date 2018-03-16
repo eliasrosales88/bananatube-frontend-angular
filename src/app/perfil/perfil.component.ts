de33649d6e9682de26cba35666342e08b1da98dd
elias <eliasrosales88@gmail.com>
2018-03-16 04:08:34 +0100

    Muestra Videos particulares de cada usuario

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -13,8 +13,16 @@ export class PerfilComponent implements OnInit {
   userId:any;
   userEmail:any;
   urlCheck:any;
+  urlVideos:any;
   dataCheck:any;
-
+  dataVideos:any;
+  videoId:any;
+  videoTitulo:any;
+  videoDesc:any;
+  videoUrl:any;
+  videoCreatedAt:any;
+  muestraVideos:boolean = false;
+  
   constructor(private http: HttpClient,private router: Router) {
     this.urlCheck="http://localhost:3000/api/loginCheck";
     this.http.get(this.urlCheck,{withCredentials: true}).subscribe(
@@ -24,10 +32,44 @@ export class PerfilComponent implements OnInit {
       this.userId = this.dataCheck.usuario._id;
       this.username = this.dataCheck.usuario.username;
       this.userEmail = this.dataCheck.usuario.email;
+      
+      
+
+
       } ,
       err => {
         console.log("Error occured");
       });  
+      
+
+
+
+
+      
+  }
+
+  getVideos(){
+    console.log(this.userId);
+    this.urlVideos = "http://localhost:3000/api/user/videos/"+ this.userId;
+      console.log(this.urlVideos);
+      this.http.get(this.urlVideos,{withCredentials: true}).subscribe(
+        dataVideos =>{
+          this.dataVideos = dataVideos;
+        console.log(this.dataVideos);
+        this.videoId = this.dataVideos._id;
+        this.videoTitulo = this.dataVideos.titulo;
+        this.videoDesc = this.dataVideos.descripcion;
+        this.videoUrl = "http://localhost:3000/"+ this.dataVideos;
+        this.videoCreatedAt = this.dataVideos.createdAt;
+        this.muestraVideos = !this.muestraVideos;
+        } ,
+        err => {
+          console.log("Error occured");
+        });
+  }
+
+  ocultaVideos(){
+    this.muestraVideos = !this.muestraVideos;
   }
 
   ngOnInit() {
